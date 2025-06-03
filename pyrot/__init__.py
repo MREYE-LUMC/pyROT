8b89dc730d4a3ea2b26dc34e54942b9908042b14
Jan-Willem Beenakker <90187768+jwmbeenakker@users.noreply.github.com>
2025-06-03 10:31:29 -0300

    add docstring to init

--- pyrot/__init__.py
+++ pyrot/__init__.py
@@ -2,4 +2,17 @@ from __future__ import annotations
 
 from pyrot import ro_interface
 
+"""
+Pyrot a package to support eye-modelling in RayOcular.
+
+Modules:
+    config: Configuration settings for the package.
+    eye_modelling: Package that assists with they eye-modelling
+    ro_interface: Provides the main interface to RaySearch/RayOcular.
+
+Public API:
+    - eye_modelling
+    - ro_interface
+"""
+
 __all__ = ["ro_interface"]
