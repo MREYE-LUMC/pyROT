d27273c29d679b22c153ee2030e29bb136dcd0df
crnh <30109443+crnh@users.noreply.github.com>
2025-06-03 16:27:51 +0200

    Fix datamodels docstring

--- pyrot/eye_modelling/datamodels/__init__.py
+++ pyrot/eye_modelling/datamodels/__init__.py
@@ -1,11 +1,10 @@
-from __future__ import annotations
-
-from pyrot.eye_modelling.datamodels import export, models, validators
-
-"""PyROT Eye Modelling Datamodels
-==============================
+"""Data structures for eye models in RayOcular.
 
 This package provides classes and functions to interact with eye models in RayOcular.
 """
+
+from __future__ import annotations
+
+from pyrot.eye_modelling.datamodels import export, models, validators
 
 __all__ = ["export", "models", "validators"]
