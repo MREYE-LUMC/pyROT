1b659d2b0bba90117097c1341e3369d736893215
Jan-Willem Beenakker <90187768+jwmbeenakker@users.noreply.github.com>
2025-06-03 10:18:05 -0300

    add module level docstrings

--- pyrot/eye_modelling/__init__.py
+++ pyrot/eye_modelling/__init__.py
@@ -1,4 +1,27 @@
 from __future__ import annotations
+"""
+eye_modelling
+=============
+
+The eye_modelling module provides methods matching the geometric eye-model to imaging and biometry data.
+
+Modules
+-------
+clipbased_model
+    Match a clip-based eye model to the clips.
+common_methods
+    Shared utility functions and methods for eye modeling.
+ellipsoid_fit
+    Functions for fitting an ellipsoid to POIs.
+match_sclera_to_markers
+    Methods for matching sclera ellipse to POIs.
+match_with_biometry
+    Methods to determine the best-fitting eye-model based on imaging and biometry data.
+
+Notes
+-----
+The methods used in this package are based on the work of Pors et al. (submitted)
+"""
 
 from pyrot.eye_modelling import (
     clipbased_model,
--- pyrot/eye_modelling/datamodels/__init__.py
+++ pyrot/eye_modelling/datamodels/__init__.py
@@ -1,4 +1,19 @@
 from __future__ import annotations
+"""
+pyROT Eye Modelling Datamodels
+======================================
+
+This package provides python datamodels for the RayOcular eye-model parameters.
+
+Submodules
+----------
+export : module
+    Functions and classes for exporting data models for RayOcular.
+models : module
+    Definitions of data models.
+validators : module
+    Utilities for validating eye models.
+"""
 
 from pyrot.eye_modelling.datamodels import export, models, validators
 
