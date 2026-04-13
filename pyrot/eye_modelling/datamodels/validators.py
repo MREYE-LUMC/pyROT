adbea7a99ba169b7091cbddc05298af4dc32ab90
crnh <30109443+crnh@users.noreply.github.com>
2026-04-13 13:01:34 +0200

    Implement JSON saving and loading for `EyeModel`  (#14)
    * Add dictionary and JSON conversion functions
    
    * Improve typing for fields
    
    * Format and add docstrings
    
    * Require RayOcular name to be a string
    
    * Copy data dictionary before modification

--- pyrot/eye_modelling/datamodels/validators.py
+++ pyrot/eye_modelling/datamodels/validators.py
@@ -4,11 +4,19 @@ from __future__ import annotations
 
 import dataclasses
 import logging
-from typing import Any, Callable, Generic, Mapping, Sequence, TypeVar
+from typing import Any, Callable, Generic, Mapping, Sequence, TypeVar, get_args
 
-__all__ = ["Vector3", "dataclass", "positive_float", "rayocular_field", "vector3"]
+__all__ = [
+    "RayOcularField",
+    "ValidationError",
+    "Vector3",
+    "dataclass",
+    "literal",
+    "optional",
+    "positive_float",
+    "vector3",
+]
 
-Instance = TypeVar("Instance")
 Value = TypeVar("Value")
 
 logger = logging.getLogger(__name__)
@@ -22,13 +30,15 @@ class ValidationError(Exception):
         super().__init__(self.message)
 
 
-class RayOcularField(Generic[Instance, Value]):
+class ValidatedField(Generic[Value]):
     """A descriptor class that provides validation for a field.
 
     Parameters
     ----------
     validator : Callable[[Any], Value]
         A callable object that performs the validation.
+    default : Value, optional
+        The default value for the field. If not provided, the field will be required and must be set explicitly.
 
     Attributes
     ----------
@@ -67,21 +77,24 @@ class RayOcularField(Generic[Instance, Value]):
     ...         self.age = age
     """
 
-    def __init__(self, validator: Callable[[Any], Value], name: str | None) -> None:
-        self.rayocular_name = name
+    def __init__(self, validator: Callable[[Any], Value], *, default: Value = ...) -> None:
         self.validator = validator
+        self._default = default
 
-    def __set_name__(self, owner: Instance, name: str):
+    def __set_name__(self, owner, name: str):
         self.public_name = name
         self.private_name = "_" + name
 
-    def __get__(self, instance: Instance, owner: type[Instance] | None = None) -> Value:
+    def __get__(self, instance, owner: type | None = None) -> Value:
+        if instance is None and self._default is not ...:
+            return self._default
+
         return getattr(instance, self.private_name)
 
-    def __set__(self, instance: Instance, value: Value):
+    def __set__(self, instance, value: Value) -> None:
         setattr(instance, self.private_name, self.validate(value))
 
-    def validate(self, value):
+    def validate(self, value: Value) -> Value:
         """Validates the given value using the provided validator.
 
         Parameters
@@ -105,28 +118,86 @@ class RayOcularField(Generic[Instance, Value]):
             raise ValidationError(value, self.public_name) from e
 
 
-# Factory function to create a validated field.
-# This is needed to make type checkers accept validated fields for type-annotated fields.
-def rayocular_field(validator: Callable[[Any], Value], name: str | None = None) -> Value:
-    """Creates a validated field with the given validator.
+class RayOcularField(ValidatedField[Value]):
+    """A descriptor class that provides validation for a RayOcular field.
 
     Parameters
     ----------
-    name : str
-        The RayOcular name of the field.
     validator : Callable[[Any], Value]
-        A callable object that takes any value as input and returns a validated value.
+        A callable object that performs the validation.
+    name : str
+        The RayOcular name of the field. This is used for serialization and deserialization to RayOcular.
+    default : Value, optional
+        The default value for the field. If not provided, the field will be required and must be set explicitly.
+
+    Attributes
+    ----------
+    public_name : str
+        The public name of the field.
+    private_name : str
+        The private name of the field.
+
+    Methods
+    -------
+    __get__(self, instance: Instance, owner: type[Instance] | None = None) -> Value:
+        Retrieves the value of the field.
+
+    __set__(self, instance: Instance, value: Value):
+        Sets the value of the field after validating it.
+
+    validate(self, value):
+        Validates the given value using the provided validator.
+
+    Raises
+    ------
+    ValueError
+        If the validation fails.
+
+    Notes
+    -----
+    Define a `ValidatedField` descriptor for a class attribute and provide a validator function.
+    The validator function should take a single argument and return the validated value.
+
+    Examples
+    --------
+    >>> class Person:
+    ...     age = ValidatedField(int)
+    ...
+    ...     def __init__(self, age):
+    ...         self.age = age
+    """
+
+    def __init__(self, validator: Callable[[Any], Value], name: str, *, default: Value = ...) -> None:
+        self.rayocular_name = name
+        self.validator = validator
+        self._default = default
+
+
+T = TypeVar("T")
+
+
+def dataclass(cls: type[T]) -> Callable[..., T]:
+    """Validator for dataclasses.
+
+    Checks if the value is an instance of the dataclass or a dict that can be used to create an instance of the dataclass.
+
+    Parameters
+    ----------
+    cls : type[T]
+        The dataclass type to validate against.
 
     Returns
     -------
-    ValidatedField[Instance, Value]
-        The validated field.
+    Callable[..., T]
+        A validator function that can be used to validate instances of the dataclass.
+
+    Raises
+    ------
+    ValueError
+        If the value is not an instance of the dataclass or a dict that can be used to create an instance of the dataclass.
     """
-    return RayOcularField(validator, name)  # type: ignore
 
-
-def dataclass(cls: type[Value]) -> Callable[..., Value]:
-    def validate(value) -> Value:
+    def validate(value) -> T:
         if isinstance(value, cls):
             return value
 
@@ -139,6 +210,23 @@ def dataclass(cls: type[Value]) -> Callable[..., Value]:
 
 
 def positive_float(value: Any) -> float:
+    """Validator for positive floats.
+
+    Parameters
+    ----------
+    value : Any
+        The value to validate.
+
+    Returns
+    -------
+    float
+        The validated positive float value.
+
+    Raises
+    ------
+    ValueError
+        If the value is not a positive float.
+    """
     if isinstance(value, float) and value >= 0:
         return value
 
@@ -147,19 +235,34 @@ def positive_float(value: Any) -> float:
 
 # Use a dataclass because of JSON serialization
 @dataclasses.dataclass(frozen=True)
-class Vector3(Generic[Value]):
-    x: Value
-    y: Value
-    z: Value
-
-
-Item = TypeVar("Item")
+class Vector3(Generic[T]):
+    x: T
+    y: T
+    z: T
 
 
 def vector3(
-    item_validator: Callable[..., Item],
-) -> Callable[[Any], Vector3[Item]]:
-    def validate(value: Any) -> Vector3[Item]:
+    item_validator: Callable[..., T],
+) -> Callable[[Any], Vector3[T]]:
+    """Validator for Vector3 objects.
+
+    Parameters
+    ----------
+    item_validator : Callable[..., T]
+        A validator function for the individual components of the vector.
+
+    Returns
+    -------
+    Callable[[Any], Vector3[T]]
+        A validator function that can be used to validate Vector3 objects.
+
+    Raises
+    ------
+    ValueError
+        If the value is not a valid Vector3 object.
+    """
+
+    def validate(value: Any) -> Vector3[T]:
         if isinstance(value, Vector3):
             return value
         if isinstance(value, Sequence):  # list-like values
@@ -174,5 +277,52 @@ def vector3(
             return Vector3(**{k: item_validator(v) for (k, v) in value.items()})
 
         raise ValueError(f"Could not parse {value=} to Vector3. `value` should be Vector3, list or dict.")
+
+    return validate
+
+
+def literal(type_: type[T]) -> Callable[[Any], T]:
+    """Validator for literal values.
+
+    Parameters
+    ----------
+    type_ : type[T]
+        The type containing the allowed literal values.
+
+    Returns
+    -------
+    Callable[[Any], T]
+        A validator function that can be used to validate literal values.
+    """
+
+    def validate(value: Any) -> T:
+        allowed = get_args(type_)
+        if value in allowed:
+            return value
+
+        raise ValueError(f"Expected one of {allowed}, got {value}.")
+
+    return validate
+
+
+def optional(inner: Callable[..., T]) -> Callable[[Any], T | None]:
+    """Validator for optional values.
+
+    Parameters
+    ----------
+    inner : Callable[..., T]
+        The validator function for the non-optional value.
+
+    Returns
+    -------
+    Callable[[Any], T | None]
+        A validator function to validate optional values.
+    """
+
+    def validate(value: Any) -> T | None:
+        if value is None:
+            return None
+
+        return inner(value)
 
     return validate
