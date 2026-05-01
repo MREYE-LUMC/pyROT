5ccff0c2ef1c2046ee9556f7ee4dada44dad0dd5
LUMCNET\ljpors <l.j.pors@lumc.nl>
2026-05-01 11:13:37 +0200

    setup structure of eyemodel_import.py

---
+++ pyrot/eye_modelling/datamodels/model_import.py
@@ -0,0 +1,35 @@
+"""Import eye model geometries to RayOcular."""
+
+# TODO:  walk through imports, assumed these should more ore less be equal to those of export.py
+
+from __future__ import annotations
+
+import logging
+
+from pyrot import ro_interface
+from pyrot.eye_modelling.datamodels.models import EyeModel
+
+logger = logging.getLogger(__name__)
+
+
+def import_eye_model(structure_set, geometry_generators, import_path):
+
+    eye_model = EyeModel.load_json(import_path)
+
+    new_values = eye_model.parameters.to_rayocular()
+
+    new_values = {
+        'ScleraThickness': 0.1,
+        # 'ScleraSemiAxis': [1.2071728589796027, 1.2436192876523449, 1.2461153979847381]
+    }
+
+    ro_interface.update_eye_model(eye_model_generators=geometry_generators, new_values=new_values)
+
+
+
+    # TODO: testen of hier hetzelfde oogmodel weer uit komt
+    # TODO: .json bestand pad uit uitgebreider mappen-directory halen
+
+
+    print(new_values)
+    print(eye_model.parameters.sclera.semi_axis)
---
+++ scripts/eyemodel_import.py
@@ -0,0 +1,26 @@
+import __common__
+
+import logging
+import sys
+
+from pyrot import ro_interface
+from pyrot.config import Config
+from pyrot.eye_modelling.datamodels import model_import
+
+# to set logging level in only this script (note that sys needs to be imported for this as well):
+logging.basicConfig(level=logging.DEBUG, handlers=[logging.StreamHandler(sys.stdout)], force=True)
+logger = logging.getLogger(__name__)
+
+logger.debug("commencing import")
+
+patient = ro_interface.load_current_patient()
+structure_set = ro_interface.load_current_structureset()
+geometry_generators, _ = ro_interface.load_eyemodel(
+    structure_set=structure_set,
+    eyemodelnr=Config.EYE_MODEL_NR)
+import_path = Config.IMPORT_PATH  # TODO: iets dat 'eyemodel' dingen die in een .json zitten uit een mapje haalt
+
+model_import.import_eye_model(structure_set, geometry_generators, import_path)
+
+
+logger.debug("import complete")
