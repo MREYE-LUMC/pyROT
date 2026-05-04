afb10c334983a74e376d8a0971ab2872e33b184f
LUMCNET\ljpors <l.j.pors@lumc.nl>
2026-05-04 11:03:23 +0200

    build framework to optionally create a new eye model that will be updated with the imported data. also, formatting. Not tested!

--- scripts/eyemodel_import.py
+++ scripts/eyemodel_import.py
@@ -11,14 +11,37 @@ from pyrot.eye_modelling.datamodels import model_import
 logging.basicConfig(level=logging.DEBUG, handlers=[logging.StreamHandler(sys.stdout)], force=True)
 logger = logging.getLogger(__name__)
 
+
+# --- user input
+
+import_method = "to_existing_eye_model"  # either 'to_existing_eye_model' or 'create_new_eye_model'
+
+# --- end user input
+
 logger.debug("commencing import")
 
 patient = ro_interface.load_current_patient()
-structure_set = ro_interface.load_current_structureset()
-geometry_generators, _ = ro_interface.load_eyemodel(structure_set=structure_set, eyemodelnr=Config.EYE_MODEL_NR)
-import_path = Config.IMPORT_PATH  # TODO: iets dat 'eyemodel' dingen die in een .json zitten uit een mapje haalt
+import_path = Config.IMPORT_PATH  # TODO: search the correct .json file from the directory
 
-model_import.import_eye_model(structure_set, geometry_generators, import_path)
+# if import_method is 'to existing eye model', we need to load the eye model to which the imported data should be applied/ registered
+if import_method == "to_existing_eye_model":
+    structure_set = ro_interface.load_current_structureset()
+    geometry_generators, _ = ro_interface.load_eyemodel(structure_set=structure_set, eyemodelnr=Config.EYE_MODEL_NR)
+
+# if import_method is 'create new eye model, we should first create an eye model, and then obtain the structure_set and geometry_generators of that eye model
+elif import_method == "create_new_eye_model":
+    logger.debug("to implement now")  # temporary logger statement
+
+else:
+    raise NotImplementedError(
+        f'not implemented: importing eye model with method {import_method}\n\
+                              please enter "to_existing_eye_model" or "create_new_eye_model"'
+    )
+
+
+# import the eye model to the relevant structure_set and geometry_generators object
+
+model_import.import_eye_model(geometry_generators, import_path)
 
 
 logger.debug("import complete")
