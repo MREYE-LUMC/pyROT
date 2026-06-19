4a2b19f85c5ec228a6f4a7b652fbeaa4a8c125bb
LUMCNET\ljpors <l.j.pors@lumc.nl>
2026-06-19 14:36:21 +0200

    format docstrings

--- pyrot/config.py
+++ pyrot/config.py
@@ -81,11 +81,11 @@ class Config:
 
      Set this to the path containing the files that should be imported.
      The import script searches this directory for files whose names match the configured import pattern.
-     """
+    """
 
     IMPORT_FILE_NAME_ELEMENT: str = "eye_model"
     """String that must be present in filenames for the import script to process them.
 
      Set this to the distinguishing part of the filenames that belong to the eye model export to import.
      The import script uses this value to filter files found in `IMPORT_DIRECTORY` and import only matching files.
-     """
+    """
